354d1f9f82ce4521a33e7ec2559ca71e59372909
znoelr <rennleonz@gmail.com>
2024-02-28 14:46:35 -0500

    Update DatabaseModule configure to use ConfigModule

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config';
+import { ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+    }),
+  ],
+})
 export class DatabaseModule {}
