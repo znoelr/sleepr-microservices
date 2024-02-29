beded29e431b0aae5ad015e7a98e3cf8c40c3577
znoelr <rennleonz@gmail.com>
2024-02-29 15:11:09 -0500

    Update DatabaseModule to accept models definitions 'forFeature'

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -3,9 +3,18 @@ import { DatabaseModule } from '@app/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { ReservationRepository } from './reservation.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationRepository],
 })
