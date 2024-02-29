a3fbac71c1f638387b05a39014ca2034fbc5aac8
znoelr <rennleonz@gmail.com>
2024-02-29 15:03:59 -0500

    Setup reservations schema and repository

---
+++ apps/reservations/src/reservation.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { AbstractRepository } from '@app/common';
+import { ReservationDocument } from './models/reservation.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ReservationRepository extends AbstractRepository<ReservationDocument> {
+  protected readonly logger: Logger = new Logger(ReservationRepository.name);
+
+  constructor(
+    @InjectModel(ReservationDocument.name)
+    reservationModel: Model<ReservationDocument>,
+  ) {
+    super(reservationModel);
+  }
+}
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@app/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
+import { ReservationRepository } from './reservation.repository';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationRepository],
 })
 export class ReservationsModule {}
