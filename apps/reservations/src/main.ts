e9027f064ed5dd8b2d9f24e8fbb02fcb50c3338c
znoelr <rennleonz@gmail.com>
2024-03-01 18:55:57 -0500

    Setup and Implement logger module

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
