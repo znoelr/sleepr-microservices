385ef9c7a88937b1f61d1561eb178d06d5ae051a
znoelr <rennleonz@gmail.com>
2024-03-01 19:51:57 -0500

    Setup validations pipes

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
