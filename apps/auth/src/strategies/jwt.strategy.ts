84b1d1ce3ec3073d05226acad7c6fd8791fa04a1
znoelr <rennleonz@gmail.com>
2024-03-04 01:34:46 -0500

    Update jwt retrieval to handle microservice requests made by microservice clients

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { UsersService } from '../users/users.service';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { TokenPayload } from '../classes/token-payload';
-import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -14,13 +13,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request?.cookies?.Authentication,
+        (request: any) =>
+          request?.cookies?.Authentication || request?.Authentication,
       ]),
       secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
   async validate({ userId }: TokenPayload) {
-    return this.userService.validateUserById(userId);
+    return await this.userService.validateUserById(userId);
   }
 }
