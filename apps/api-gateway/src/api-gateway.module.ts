c086c847adf884c0fcb3fc72e813b244d3377dfc
znoelr <rennleonz@gmail.com>
2024-03-10 18:14:56 -0500

    Setup gateway config, and add subgraph for reservations graphql

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from '@app/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
   imports: [
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              {
+                name: 'reservations',
+                url: configService.getOrThrow('RESERVATIONS_GRAPHQL_URL'),
+              },
+            ],
+          }),
+        },
+      }),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
